b536a92c5be092451b2fd4c95402d45a2a8f4038
flytcher <stevenfletcher17@gmail.com>
2023-03-01 22:50:41 -0700

    Add project files.

---
+++ Mission9__stevenf4/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission9__stevenf4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission9__stevenf4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookRepository repo;
+
+        public HomeController (IBookRepository temp)
+        {
+            repo = temp;
+        }
+       
+        public IActionResult Index()
+        {
+            var blah = repo.Books.ToList();
+
+            return View();
+        }
+    }
+}
